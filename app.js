34c3ab3e963e73a6885d7bd0856aae6a67ba653b
ashupn10 <ashupn10@gmail.com>
2023-01-09 16:35:42 +0530

    Added contact page

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const app=express();
 
 const router=require('./routes/login');
 const router2=require('./routes/messages');
+const router3=require('./routes/contact.js');
 const bodyParser=require('body-parser');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:false}))
 app.use('/login',router);
 app.use('/messages',router2);
+app.use('/contact',router3)
 app.use((req,res,next)=>{
     res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 })
