beb8198d42695720cf81318b3980f34bdd2b5ca8
ashupn10 <ashupn10@gmail.com>
2023-01-10 12:56:48 +0530

    Added Controllers

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,9 @@
 const express=require('express');
+const controls=require('../controllers/userMessages');
 const router=express.Router();
 const path=require('path');
 const bodyParser=require('body-parser');
 bodyParser.urlencoded({extended:false});
-router.use((req,res,next)=>{
-    res.sendFile(path.join(__dirname,'..','views','login.html'));
-})
+router.use(controls.sendLogin);
 
 module.exports=router;
